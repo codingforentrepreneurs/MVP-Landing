a3c1b78fbcfb02a6cfcf339d0343a978ac2ef98f
CFE <hello@teamcfe.com>
2020-07-23 15:30:35 -0700

    9 - Making Model Changes

--- emails/models.py
+++ emails/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class EmailEntry(models.Model):
     email = models.EmailField()
+    name = models.CharField(max_length=120, blank=True)
+    bio = models.TextField(blank=True)
+    # date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True) # set when saved?
+    timestamp = models.DateTimeField(auto_now_add=True) # set when added?
