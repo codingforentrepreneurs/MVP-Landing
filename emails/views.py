41c1945904f1824778fd219a0fff33a704819988
CFE <hello@teamcfe.com>
2020-07-23 21:29:50 -0700

    11 - Your first view function

--- emails/views.py
+++ emails/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# Model -> View -> Template
+
+def email_entry_get_view(request, *args, **kwargs):
+    # get a single item stored in the database
+    obj = EmailEntry.objects.get(id=1)
+    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+
+# def email_entry_list_view():
+#     return
+
+# def email_entry_create_view():
+#     return
+
+# def email_entry_update_view():
+#     return
+
+# def email_entry_destroy_view():
+#     return
