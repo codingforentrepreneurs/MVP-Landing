f976fcbd9c1b268df307d7706bc531a428192e68
CFE <hello@teamcfe.com>
2020-07-27 10:24:49 -0700

    14 - Setting up Django Templates

--- emails/views.py
+++ emails/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 # Create your views here.
 # Model -> View -> Template
+# Model -> View -> React.js / Vue.js
 
 from .models import EmailEntry
 
+
+html_str = "<!doctype html><html><body><h1>{email}</body></html>"
 
 def email_entry_get_view(request, id=None, *args, **kwargs):
     # get a single item stored in the database
@@ -14,7 +17,9 @@ def email_entry_get_view(request, id=None, *args, **kwargs):
         obj = EmailEntry.objects.get(id=id)
     except EmailEntry.DoesNotExist:
         raise Http404
-    return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+    # my_html = html_str.format(email=obj.email)
+    # return HttpResponse(f"<h1>Hello {obj.email}</h1>")
+    return render(request, "get.html", {"object": obj, "email": "abc@gmail.com"})
 
 # def email_entry_list_view():
 #     return
